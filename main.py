5f9d0bd2ff5b5462aaac7d5c4ab791ea6032b468
unknown <telewolf653@gmail.com>
2021-09-22 01:40:21 +0330

    fix bugs

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from time import sleep
 #pyrogram.errors.exceptions.bad_request_400.UsernameInvalid: [400 USERNAME_INVALID]: The username is invalid (caused by "contacts.ResolveUsername
 #pyrogram.errors.exceptions.bad_request_400.UserAlreadyParticipant: [400 USER_ALREADY_PARTICIPANT]: The user is already a participant of this chat (caused by "messages.ImportChatInvite"
 #pyrogram.errors.exceptions.flood_420.FloodWait [420 FLOOD_WAIT_X]
-LINK = 'https://t.me/project_gp_nahad'
+
+LINK = 'https://t.me/joinchat/8cEvVi1qCzM2NDVk'
 TEXT = 'سلام'
-MAX_SEND_PER_SESSION = 1
+MAX_SEND_PER_SESSION = 2
 
 
 
@@ -35,10 +36,10 @@ def get_group_members(session,group_id):
     print(group_id)
     with open('usernames.txt' , 'w') as f:
         with Client(session) as app:
-            members_logs = app.iter_chat_members(chat_id = group_id,limit = 2)
+            members_logs = app.iter_chat_members(chat_id = group_id)
             for member in members_logs:
-                print(member.user.first_name)
-                f.write(f'{member.user.id}\n')
+                if(not member.user.is_bot):
+                    f.write(f'{member.user.id}\n')
                 #member_list.append(member.user.id)
 
     #return member_list
@@ -48,13 +49,13 @@ def get_group_members(session,group_id):
 
 
 
-def join_chat(session_list):
+def join_chat(session_list,link):
     
-   
-    res = re.findall(r'^.*t\.me/(\w*)$',LINK)
+    res = re.findall(r'^.*t\.me/(\w*)$',link)
 
     if(len(res)):
         link = res[0]
+    
 
     for session in session_list:
         with Client(session) as app:
@@ -87,15 +88,16 @@ def join_chat(session_list):
     
     
 
-def send_message_to_members(session_list):
+def send_message_to_members(session_list,text,max_send):
     with open('usernames.txt' , 'r+') as f:
         user_ids = f.readlines()
         for session in session_list:
             with Client(session) as app:
                 tedad = 0 #==> tedade bedon=one error
-                for i in range(0,MAX_SEND_PER_SESSION):
+                for i in range(0,max_send):
                     user_id = user_ids[i].replace('\n' , '')
-                    app.send_message(chat_id = user_id , text = TEXT)
+                    app.send_message(chat_id = user_id , text = text)
+                    print(user_id)
                     tedad += 1
 
             f.seek(0)
@@ -105,14 +107,15 @@ def send_message_to_members(session_list):
 
 
 session_list = get_session_list()
+#group_id = join_chat(session_list,LINK)
 try:
-    group_id = join_chat(session_list)
+    group_id = join_chat(session_list,LINK)
 except UnboundLocalError:
     print('dar join shodan moshkeli vojood darad....\nlotfan barasi konid!')
 else:
     get_group_members(session_list[0] ,group_id)
 
-send_message_to_members(session_list)
+send_message_to_members(session_list,TEXT,MAX_SEND_PER_SESSION)
 
 #print(group_members)
 
