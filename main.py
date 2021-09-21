88a0d431e24cda9d90954ca0df25c9f76091842f
unknown <telewolf653@gmail.com>
2021-09-21 22:30:59 +0430

    get group members + join)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyrogram import Client
 import sqlite3
 import re
 from pyrogram.errors import BadRequest,FloodWait
+from time import sleep
 #pyrogram.errors.exceptions.bad_request_400.UsernameInvalid: [400 USERNAME_INVALID]: The username is invalid (caused by "contacts.ResolveUsername
 #pyrogram.errors.exceptions.bad_request_400.UserAlreadyParticipant: [400 USER_ALREADY_PARTICIPANT]: The user is already a participant of this chat (caused by "messages.ImportChatInvite"
 #pyrogram.errors.exceptions.flood_420.FloodWait [420 FLOOD_WAIT_X]
@@ -29,9 +30,17 @@ def get_session_list():
     return session_list
 
 
-# def get_group_members(session):
-#     with Client(session) as app:
-#         Client
+def get_group_members(session,group_id):
+    member_list = []
+    with Client(session) as app:
+        members_logs = app.iter_chat_members(chat_id = group_id)
+        for member in members_logs:
+            member_list.append(member.user.id)
+
+    return member_list
+        
+        
+    
 
 
 
@@ -42,7 +51,7 @@ def join_chat(session_list,link):
     for session in session_list:
         with Client(session) as app:
             try:
-                app.join_chat(link)
+                join = app.join_chat(link)
             except BadRequest as e:
                 id = e.ID
                 if(id == 'USERNAME_INVALID'):
@@ -52,15 +61,21 @@ def join_chat(session_list,link):
             except FloodWait as e:
                 sleep_time = e.x
                 print(f'{session} bayad {sleep_time} sanie esterahat kone!')
+                sleep(sleep_time)
             except:
                 print('error')
             else:
                 print(f'{session} be grouh add shod!')
 
+    
+
+    return join.id
+
 
 session_list = get_session_list()
-join_chat(session_list,LINK)
-#group_members = get_group_members(session_list[0])
+group_id = join_chat(session_list,LINK)
+group_members = get_group_members(session_list[0] ,group_id)
+print(group_members)
 
 
 
